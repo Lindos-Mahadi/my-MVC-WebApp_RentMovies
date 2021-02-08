357753edf5052d00c73c697c9e9630684dc4e674
lindo <lindo@LINDOS>
2021-02-08 19:04:25 +0600

    Added Movie Model

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp_RentMovies.Models;
+
+namespace WebApp_RentMovies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek !"
+            };
+            return View(movie);
+        }
+    }
+}

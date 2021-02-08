05da5fb5fd8c663ec0b997ee6eb2bdbb164a9a00
lindo <lindo@LINDOS>
2021-02-08 15:10:03 +0600

    RentMovies-Project Initialize

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_RentMovies.Startup))]
+namespace WebApp_RentMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
